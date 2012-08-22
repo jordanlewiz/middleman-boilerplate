53d63242c04bc70c8a2ad774dcdc97d1f04e345d
Jordan Lewis <jordan@involved.com.au>
2012-08-23 08:17:33 +1000

    Cleaned up old project files

--- config/routes.rb
+++ config/routes.rb
@@ -3,80 +3,15 @@ module RouteHelpers
     "/"
   end
 
-  def how_it_works_path
-    "/how-it-works"
+  def page_path
+    "/page-name"
   end
 
-  def events_path
-    "/member-events"
-  end
-
-  def speaker_path(slug)
-    "/member-events/#{slug}.html"
-  end
-
-  def entrepreneurs_path
-    "/entrepreneurs"
-  end
-
-  def story_path(slug)
-    "/entrepreneurs/#{slug}.html"
-  end
-
-  def angels_path
-    "/angels"
+  def templated_page_path(slug)
+    "/page-name/#{slug}.html"
   end
 
   def membership_path(page)
-    "/membership/#{page}"
-  end
-
-  def blog_path
-    "/baywatch"
-  end
-
-  def about_path
-    "/who-are-we"
-  end
-
-  def contact_path
-    "/contact-us"
-  end
-
-  def become_sponsor_path
-    "/become-a-sponsor"
-  end
-
-  def tcs_path
-    "/the-small-print"
-  end
-
-  def video_pitch_path
-    "#"
-  end
-
-  def info_mailto
-    "info@innnovationbay.com"
-  end
-
-  def twitter_path
-    "http://www.twitter.com/innovationbay"
-  end
-
-  def sponsorship_pdf_download_path
-    "/downloads/innovation_bay-sponsorship_prospectus-2012.pdf"
-  end
-
-  def sponsor_path(company)
-    case company
-      when "deloitte"
-        "http://www.deloitte.com.au"
-      when "ninefold"
-        "http://www.ninefold.com.au"
-      when "nortonrose"
-        "http://www.nortonrose.com.au"
-      else
-        "#"
-      end
+    "/page-name/#{page}"
   end
 end
