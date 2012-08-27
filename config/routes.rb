80d0f70345a5b60cfba21df09ba4c98054ceeb74
Jordan Lewis <jordan@involved.com.au>
2012-08-28 09:17:57 +1000

    Added example structure with sub pages

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,33 @@ module RouteHelpers
     "/page-name/#{slug}.html"
   end
 
-  def membership_path(page)
+  def page_path(page)
     "/page-name/#{page}"
+  end
+
+
+  def pages_for_group(group_name)
+    group = data.nav.find do |g|
+      g.name == group_name
+    end
+      
+    pages = []
+    
+    return pages unless group
+    
+    if group.directory
+      pages << sitemap.resources.select { |r|
+        r.path.include?(group.directory) && !r.data.hidden
+      }.map do |r|
+        ::Middleman::Util.recursively_enhance({
+          :title => r.data.title,
+          :path  => r.url 
+        })
+      end.sort_by { |p| p.title }
+    end
+    
+    pages << group.pages if group.pages
+    
+    pages.flatten
   end
 end
